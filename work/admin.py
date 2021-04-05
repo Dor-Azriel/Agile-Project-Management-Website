e5c12f0c0cabcb8518c6ee20260853d883e96577
Dor Azriel <doraz4.ac.sce.ac.il>
2021-04-05 20:44:29 +0300

    task add page.

--- work/admin.py
+++ work/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import skill
 from .models import SubTask
+from .models import Task
 
 admin.site.register(skill)
 admin.site.register(SubTask)
+admin.site.register(Task)
