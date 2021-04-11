bc82092e4de837ef68675d34c32d8d309d23c178
Dor Azriel <doraz4.ac.sce.ac.il>
2021-04-11 15:20:40 +0300

    admin projects view

--- work/admin.py
+++ work/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import skill
 from .models import SubTask
 from .models import Task
+from .models import project
 
 admin.site.register(skill)
 admin.site.register(SubTask)
 admin.site.register(Task)
+admin.site.register(project)
