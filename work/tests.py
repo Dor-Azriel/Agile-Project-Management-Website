d619d745721f9cd052b03540669f787d3dfb578c
Dor Azriel <doraz4.ac.sce.ac.il>
2021-05-06 17:06:49 +0300

    jenkins mail try

--- work/tests.py
+++ work/tests.py
@@ -1,10 +1,3 @@
 from django.test import TestCase
 
 # Create your tests here.
-from work.models import skill
-
-
-class TestModels(TestCase):
-    def user_create(self):
-        self.tmp=skill.objects.create(language="JA",level="JU")
-       # self.assertEqual(str(lang),"JAVA")
