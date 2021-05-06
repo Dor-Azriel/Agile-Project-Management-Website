f821375d9dc2de55bf9fa1a4959774c5193add99
Dor Azriel <doraz4.ac.sce.ac.il>
2021-05-06 11:22:52 +0300

    Tests try

--- work/tests.py
+++ work/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+from work.models import skill
+
+
+class TestModels(TestCase):
+    def user_create(self):
+        self.tmp=skill.objects.create(language="JA",level="JU")
+       # self.assertEqual(str(lang),"JAVA")
