17ab215090eb2236eb65d91d33cb532344fe178a
Dor Azriel <doraz4.ac.sce.ac.il>
2021-05-06 12:51:28 +0300

    Unit tests models

--- work/test_models.py
+++ work/test_models.py
@@ -1,8 +1,28 @@
 from unittest import TestCase
 from work.models import skill
-
+from work.models import review
+from work.models import Task
+from datetime import datetime
 
 class Testskill(TestCase):
     def test_is_upperclass(self):
         self.tmp = skill.objects.create(language="JA", level="JU")
-        self.assertEqual(str(self.tmp.language),"JA")
+
+    def is_equal(self):
+        TestCase.failUnlessEqual(self.tmp.level, "JU")
+        TestCase.failUnlessEqual(self.tmp.language, "JA")
+        #TestCase.fail
+
+class TestTask(TestCase):
+    def init(self):
+        self.tmp=Task.objects.create(startTime=datetime(2020,4,4)
+                                     ,endTime=datetime(2020,4,5)
+                                     ,inCharge=1,lastUpdate=datetime.now()
+                                     ,cost=1000
+                                     , subTasks=1,
+                                     TaskName="Test"
+                                     ,Description="testing")
+
+class Testreview(TestCase):
+    def init(self):
+        self.new=review.objects.create(ProjectName="TEST",Testreview="TEST",TaskReview=1)
