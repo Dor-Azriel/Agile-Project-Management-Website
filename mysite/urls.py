ca07a0228083201f801c6666d83cd92d396b2e46
Dor Azriel <doraz4.ac.sce.ac.il>
2021-05-20 13:17:36 +0300

    full login

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,9 +17,11 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path,include
 from pages.views import task_views,add_comment, task_sorted_by_date, home_view, task_sorted_by_done, task_detail,dynamic_view
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('', home_view),
+    path('login/',LoginView.as_view(template_name='admin/login.html')),
     path('tasks/', task_views,name='tasks_views'),
     path('admin/', admin.site.urls),
     path('sort_date/', task_sorted_by_date),
