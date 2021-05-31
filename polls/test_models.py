0b7874bf4ef1e8040e547e631ea639898f9b866d
Dor Azriel <doraz4.ac.sce.ac.il>
2021-05-31 16:41:12 +0300

    unit test

--- polls/test_models.py
+++
@@ -1,7 +0,0 @@
-from unittest import TestCase
-from polls.models import Question
-from datetime import datetime
-class TestQuestion(TestCase):
-    def test_was_published_recently(self):
-        tmp =Question.objects.create(question_text="Test",pub_date=datetime(2020, 4, 4))
-        #tmp.was_published_recently()
